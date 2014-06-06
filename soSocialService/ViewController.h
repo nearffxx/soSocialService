81ad83f6e9a98cc47efecc18791b50ba99ba750b
nearffxx <nearffxx@hotmail.com>
2014-06-06 16:56:20 +0200

    Initial Commit

---
+++ soSocialService/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  soSocialService
+//
+//  Created by nearffx on 06/06/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ soSocialServiceTests/soSocialServiceTests.m
@@ -0,0 +1,34 @@
+//
+//  soSocialServiceTests.m
+//  soSocialServiceTests
+//
+//  Created by nearffx on 06/06/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface soSocialServiceTests : XCTestCase
+
+@end
+
+@implementation soSocialServiceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
